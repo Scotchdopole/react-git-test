ae601789b7e9a1cab9a24d665acf39feb253f795
Kapp1cka <simon.soltes07@gmail.com>
2025-02-20 10:10:37 +0100

    nova stranka

--- src/pages/AppRoutes.jsx
+++ src/pages/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import VtipnaStranka from './VtipnaStranka/VtipnaStranka'
+import RetarPage from './RetarPage'
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/vtipnastranka' element={<VtipnaStranka/>} />
+            <Route path='/retarpage' element={<RetarPage/>} />
         </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/RetarPage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RetarPage() {
+  return (
+    <>
+        <p>tohle udelal kapp1cka</p>
+    </>
+  )
+}
